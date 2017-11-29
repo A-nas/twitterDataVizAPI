28ca914db2c045366bbedf4fffcf80375de147b0
A-nas <anass.laghouaouta@hotmail.fr>
2017-11-30 00:56:04 +0100

    first test, API is up

--- src/AppBundle/Controller/TwitterController.php
+++ src/AppBundle/Controller/TwitterController.php
@@ -5,14 +5,19 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\View\View;
 
 class TwitterController extends Controller
 {
 	// careful ! any route anotation added here will be ignored
-    public function getWordsAction($type, Request $request)
-    {
-        // replace this example code with whatever you need
-        return new JsonResponse('{ "hello" : "word"}');
+    public function getWordsAction($type, Request $request){
+    	$viewHandler = $this->get('fos_rest.view_handler');
+    	// Création d'une vue FOSRestBundle
+        $view = View::create('{ "hello" : "word"} ');
+        $view->setFormat('json');
+        // Gestion de la réponse
+        return $viewHandler->handle($view);
+        //return new JsonResponse('{ "hello" : "word"}');
     }
 }
 
